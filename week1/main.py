9291281a83b9cc543b4602e88ec0d3bf3cf9525e
Gociu Radu <radugociu89@gmail.com>
2025-03-28 21:30:10 +0200

    1

--- week1/main.py
+++ week1/main.py
@@ -1,1 +1,13 @@
+from src.utils import load_dataset
 
+def main():
+    data = load_dataset("diabetes_dataset.csv")
+    target="diabet"
+    train_size=int(0.8*len(data))
+    train_data=data[:train_size]
+    test_data=data[train_size:]
+    print("train data: ",train_data.shape)
+    print("test data: ",test_data.shape)
+
+if __name__ == "__main__":
+    main()
--- week1/src/id3.py
+++ week1/src/id3.py
@@ -1,1 +1,10 @@
+class Node :
+    def __init__(self,column,value=None,true_branch=None,false_branch=None,label=None):
+        self.column=column
+        self.value=value
+        self.true_branch=true_branch
+        self.false_branch=false_branch
+        self.label=label
 
+def is_leaf :
+    return self.lable is not None
